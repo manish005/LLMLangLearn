4e53e78e39193a88b1d3f8a5798e32376f80b692
manish005 <44979844+manish005@users.noreply.github.com>
2023-11-27 00:45:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #Hello! It seems like you want to import the Streamlit library in Python. Streamlit is a powerful open-source framework used for building web applications with interactive data visualizations and machine learning models. To import Streamlit, you'll need to ensure that you have it installed in your Python environment.
 #Once you have Streamlit installed, you can import it into your Python script using the import statement,
-pip install langchain
+
 import streamlit as st
 
 
